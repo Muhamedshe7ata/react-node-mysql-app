4470261e7305ab94e8bce68b58b72461f49b3d6b
MohamedSheh7ata <mohammedalqttan@gmail.com>
2025-06-09 17:02:50 +0300

    new code apptsx4

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+
+
+interface ImportMetaEnv {
+  readonly REACT_APP_API_URL: string;
+  // Add any other environment variables you might use in your frontend
+  // readonly VITE_ANOTHER_VAR: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
